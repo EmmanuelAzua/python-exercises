bf4f8ff19e2a93ef335636d743f7f72f956fdfef
Emmanuel Azua <emmanuel.a.rivera.a@gmail.com>
2021-04-06 17:05:46 -0500

    An update to the file CAO 06 aPR 21, 1707 CST

---
+++ data_types_and_variables.py
@@ -0,0 +1,18 @@
+# This is my first PYTHON independent practice exercise file
+
+99.9
+type(99.9)
+
+type(False)
+
+# What data type would best represent:
+
+# A term or phrase typed into a search box?
+# If a user is logged in?
+# A discount amount to apply to a user's shopping cart?
+# Whether or not a coupon code is valid?
+# An email address typed into a registration form?
+# The price of a product?
+# A Matrix?
+# The email addresses collected from a registration form?
+# Information about applicants to Codeup's data science program?
